c59ee3e1376677ede7890e012d3215a9eaa811a5
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-04-12 17:05:31 -0300

    ,m/,

---
+++ Exercicio2/Exercicio2.cpp
@@ -0,0 +1,64 @@
+//EXERCICIO 1
+
+#include <iostream> 
+using namespace std;
+int main(int argc, char* argb[]) {
+	/*	int unidade = 25;
+		float igualVinteCinco = 1.50;
+		float AcimaDeVinteCinco = 1.25;
+
+
+		if (unidade <= 25) {
+			float mult = unidade * igualVinteCinco;
+			cout << mult;
+		}
+		else if(unidade >=26) {
+			float mult = unidade * AcimaDeVinteCinco;
+			cout << mult;
+		}
+		return 0;
+	}*/
+
+	//EXERCICIO 2
+	/*int numero = 10;
+
+
+		if (numero >= 10) {
+			int soma, mult = numero + 10 * 2;
+			cout << soma, mult;
+		}
+		else if (numero <= 9) {
+			int soma, mult, sub = numero + 9 * 3 - 8;
+			cout << soma, mult, sub;
+		}
+		return 0;*/
+
+
+
+		//EXERCICIO 3
+
+		/*int numero1 = 0;
+
+
+		if (numero1 >= 0 ) {
+			cout << "positivo";
+		}
+		else if (numero1 <= 0) {
+			cout << "negativo";
+		}*/
+	return 0;
+
+
+	//EXERCICIO 4
+
+	int numero = 6;
+	int numero2 = 9
+
+		if (numero <= 6) {
+			cout << "Numero2 é maior";
+		}
+		else {
+
+		}
+
+}
